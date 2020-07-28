4ab826491f588b2547415fa5bd05083725035fc8
VladimirSlesaev <vovaslesarev@gmail.com>
2020-07-28 16:50:45 +0300

    adding links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import Header from './components/header/header.compohent';
 import './App.css';
 import ShopPage from './pages/shop/shop.component';
 import HomePage from './pages/homepage/homepage.component';
@@ -9,6 +9,7 @@ import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
